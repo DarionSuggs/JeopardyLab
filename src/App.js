2d27a24ce143e5a0d8264b2264cc962b0045914e
Darion Suggs <dariontsuggs@icloud.com>
2020-04-05 17:55:15 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createBrowserHistory } from "history";
+import React from "react";
+// import { Route, Switch } from "react-router-dom"
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+import Navigation from "./components/navigation/Navigation";
+import Jeopardy from "./components/jeopardy/Jeopardy";
+import { Route, Switch, Router } from "react-router-dom";
+
+const history = createBrowserHistory();
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Navigation />
+        <Switch>
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} component={Welcome} />}
+        />
+        <Route exact path="/clock" component={Clock} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/jeopardy" component={Jeopardy} />
+        </Switch>
+      </Router>
     </div>
   );
 }
